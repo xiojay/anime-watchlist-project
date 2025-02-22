b3729d53ffcb386050cef958cd5803d229eadc7d
xiojay <jocey.ale24@gmail.com>
2025-02-22 10:15:07 -0500

    auth updates

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,15 +1,21 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from . import views  # Import views to connect routes to view functions
-
+from . import views  
 urlpatterns = [
+    # home & about
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+
+    # shows
     path('shows/', views.shows_index, name='shows-index'),
     path('shows/<int:anime_id>/', views.anime_detail, name='anime-detail'),
+    path('shows/create/', views.AnimeCreate.as_view(), name='show-create'), 
+
+    # authentication
+    path('signin/', views.signin, name='signin'), 
+    path('signup/', views.signup, name='signup'),  
 ]
 
-# ✅ Add this line correctly outside the urlpatterns list
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import get_object_or_404, render
-from .models import Anime  # Import the Anime model
+from django.views.generic.edit import CreateView
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+
+from .models import Anime 
 
 def home(request):
     return render(request, 'home.html')
@@ -8,14 +13,41 @@ def about(request):
     return render(request, 'about.html')
 
 def shows_index(request):
-    query = request.GET.get('q', '')
+    query = request.GET.get('q', '') 
     if query:
-        shows = Anime.objects.filter(title__icontains=query)
+        shows = Anime.objects.filter(title__icontains=query).order_by('-rating') 
     else:
-        shows = Anime.objects.all()
+        shows = Anime.objects.all().order_by('-rating')  
 
     return render(request, 'shows/index.html', {'shows': shows, 'query': query})
 
 def anime_detail(request, anime_id):
-    anime = Anime.objects.get( id=anime_id)
+    anime = get_object_or_404(Anime, id=anime_id) 
     return render(request, 'shows/details.html', {'anime': anime})
+
+class AnimeCreate(CreateView):
+    model = Anime
+    fields = '__all__'  
+    template_name = 'shows/anime_form.html'  
+    success_url = '/shows/' 
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user) 
+            return redirect('home')  
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup/signup.html', {'form': form})
+
+def signin(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user) 
+            return redirect('home') 
+    else:
+        form = AuthenticationForm()
+    return render(request, 'signin/signin.html', {'form': form})
